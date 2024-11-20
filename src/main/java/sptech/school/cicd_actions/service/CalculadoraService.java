c86aa1faaefec3c6adbc5d3169906d84a50f6455
Isabel Alberti <isabel.fagundes@sptech.school>
2024-11-19 21:55:00 -0300

    Correção

--- src/main/java/sptech/school/cicd_actions/service/CalculadoraService.java
+++ src/main/java/sptech/school/cicd_actions/service/CalculadoraService.java
@@ -4,9 +4,8 @@ public class CalculadoraService {
         if(b==0){
             throw new ResponseStatusException(
                 HttpStatus.BAD_REQUEST,
-                "Divisão por zero não permitida"
-            )
-        }
+                "Divisão por zero não permitida");
+            }
            return a/b;
     }
 }
